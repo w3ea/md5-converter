6dfa283b80b3d9c802773125c39d48329f1cd995
ehsan <ehsanabttahi@gmail.com>
2021-02-12 15:43:04 +0330

    install and Configuring Tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import type { AppProps } from 'next/app';
+import '../assets/styles/main.scss';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
     // eslint-disable-next-line react/jsx-props-no-spreading
