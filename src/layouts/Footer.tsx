db9c247d65ddd968af6e7086d687492b334ff130
ehsan <ehsanabttahi@gmail.com>
2021-02-15 11:01:30 +0330

    Add title and info to the footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,8 +1,16 @@
 import { FC } from 'react';
 
 const Footer: FC = () => (
-    <footer className='bg-gray-600'>
-        <div className='h-2.5' />
+    <footer className='bg-gray-700'>
+        <div className='container flex justify-between items-center py-.5'>
+            <p className='text-gray-300 text-3.5'>
+                MD5 Converter
+            </p>
+            <div className='flex items-center'>
+                <span className='text-4.5 text-gray-300'>&#169;</span>
+                <span className='ml-.5 text-3.5 text-gray-300'>2021</span>
+            </div>
+        </div>
     </footer>
 );
 
